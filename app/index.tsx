a6c42638c71f7f0ff780183fa693b8867306da70
angr12 <rla120@ic.ac.uk>
2024-07-14 13:19:58 +0100

    Added ability to add task

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,22 @@
-import { Text, View, StyleSheet } from "react-native";
+import { 
+  Text, 
+  View, 
+  StyleSheet, 
+  KeyboardAvoidingView, 
+  TextInput, 
+  TouchableOpacity, 
+  Platform } from "react-native";
 import Tasks from "@/components/tasks";
+import React, { useState } from "react";
 
 export default function Index() {
+  const [task, setTask] = useState<string>(''); // Change the task name
+
+  const handleAddTask = () => {
+    console.log(task);
+  }
+
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tasks:</Text>
@@ -10,15 +25,31 @@ export default function Index() {
         <Tasks name="Task 1"/>
         <Tasks name='Task 2'/>
       </View>
+
+      {/* Add new tasks */}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+        >
+        <TextInput style={styles.input} placeholder={'Write a task'}
+        value={task}
+         onChangeText={text => setTask(text)}/>
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.addButton}>
+            <Text>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    padding: 20,
+    paddingHorizontal: 20,
+    paddingTop: 70,
   },
   title: {
     fontSize: 24,
@@ -27,4 +58,29 @@ const styles = StyleSheet.create({
   items: {
     marginTop: 30,
   },
+  writeTaskWrapper:{
+    flexDirection: 'row',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    position: 'absolute',
+    bottom: 40,
+    marginBottom: 10,
+  },
+  input:{
+    paddingHorizontal: 10,
+    paddingVertical: 15,
+    backgroundColor: '#f8f8f8',
+    width: 250,
+    borderRadius: 30,
+    borderWidth: 1,
+  },
+  addButton:{
+    width: 50,
+    paddingVertical: 15,
+    borderWidth: 1, 
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
   });
--- components/tasks.tsx
+++ components/tasks.tsx
@@ -1,10 +1,41 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 
 export default function Tasks({ name }: { name: string }) {
     return(
-        <View>
-            <Text>{name}</Text>
+        <View style={styles.item}>
+            <View style={styles.align}>
+                <TouchableOpacity style={styles.button}></TouchableOpacity>
+                <Text style={styles.text}>{name}</Text>
+            </View>
+            <View>
+
+            </View>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    item:{
+        backgroundColor: '#f8f8f8',
+        padding: 15,
+        borderRadius: 10,
+        marginBottom: 15,
+        justifyContent: 'space-between',
+    },
+    button:{
+        width: 24,
+        height: 24,
+        marginRight: 15,
+        backgroundColor: '#55bcf6',
+        opacity: 0.6,
+    },
+    align:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        flexWrap: 'wrap',
+    },
+    text:{
+        maxWidth: '80%', 
+    }
+});
